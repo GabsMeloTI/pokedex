5d384041573c1d9f7d8e7b495b9b44801941ebdd
gabsmelin <rm550202@fiap.com.br>
2024-01-04 20:49:45 -0300

    dados pokémon estão voltando da api e sendo mostrados na tela.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
+import { useState } from 'react';
 import './App.scss';
 import pokedex from './img/pokedex.png'
 
 export default function App() {
+  const [nome, setNome] = useState('');
+  const [numero, setNumero] = useState('');
+  const [imagem, setImagem] = useState('');
+  const [pesquisa, setPesquisa] = useState('');
+
+  const fetchPokemon = async(pokemon) => {
+      const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+      const dados = await APIResponse.json();
+      return dados;
+  }
+  
+  const renderPokemon = async(pokemon) => {
+    const dados = await fetchPokemon(pokemon);
+    setNome(dados.name);
+    setNumero(dados.id);
+    setImagem(dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default']);
+  }
+  renderPokemon('1')
+
   return (
     <div className="container">
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/25.png" alt="pokemon" className='pokemon-img'/>
+      <img src={imagem} alt="pokemon" className='pokemon-img'/>
       
       <h1 className='dados-pokemon'>
-        <span className='numero-pokemon'>25 - </span>  
-        <span className='nome-pokemon'>Pikachu</span>
+        <span className='numero-pokemon'>{numero} - </span>  
+        <span className='nome-pokemon'>{nome}</span>
       </h1>
       
       <form className='form'>
-        <input type="search" placeholder='nome ou número' className='input_pesquisa' id='input' required/>
+        <input type="search" placeholder='nome ou número' className='input_pesquisa' id='input' required value={pesquisa}/>
       </form>
 
       <div className='buttons'>
