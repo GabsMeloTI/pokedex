4a1f6ea8585d4d7c966b4f5e73f4e1a69da63e52
gabsmelin <rm550202@fiap.com.br>
2024-01-04 20:01:21 -0300

    elemento de img e dados do pokemon adicionados.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.scss';
+import pokedex from './img/pokedex.png'
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/25.png" alt="pokemon" className='pokemon-img'/>
       
+      <h1 className='dados-pokemon'>
+        <span className='numero-pokemon'>25</span> -
+        <span className='name-pokemon'>Pikachu</span>
+      </h1>
+      
+      <img src={pokedex} alt="Pokedex" className='pokedex'/>
     </div>
   );
 }
