29e42233f70a8ebb2142d5e51234447d1add8cea
gabsmelin <rm550202@fiap.com.br>
2024-01-04 21:47:28 -0300

    Projeto quase finalizado, faltando apenas a incrementação através dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,58 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import pokedex from './img/pokedex.png'
+import erro from './img/download.png'
 
 export default function App() {
   const [nome, setNome] = useState('');
   const [numero, setNumero] = useState('');
   const [imagem, setImagem] = useState('');
   const [pesquisa, setPesquisa] = useState('');
+  let numeroPokemon = 1;
 
   const fetchPokemon = async(pokemon) => {
       const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-      const dados = await APIResponse.json();
-      return dados;
+      if(APIResponse.status === 200) {
+        const dados = await APIResponse.json();
+        return dados;
+      }
   }
   
   const renderPokemon = async(pokemon) => {
+    setNumero(0);
+    setNome("Loading...");
     const dados = await fetchPokemon(pokemon);
-    setNome(dados.name);
-    setNumero(dados.id);
-    setImagem(dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default']);
+
+    if(dados) {
+      setNome(dados.name);
+      setNumero(dados.id);
+      setImagem(dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default']);
+    } else {
+      setImagem()
+      setNumero("404");
+      setNome("Não encontrado")
+      setImagem(erro)
+    }
   }
-  renderPokemon('1')
+
+  useEffect(() => {
+    renderPokemon(numeroPokemon);
+  }, []);
+
+  const handleChange = (e) => {
+    setPesquisa(e.target.value);
+  }
+
+  const handleClickMais = () => {
+    numeroPokemon += 1;
+    renderPokemon(numeroPokemon)
+  }
+  const handleClickMenos= () => {
+    numeroPokemon -= 1;
+    renderPokemon(numeroPokemon)
+  }
+
+  
 
   return (
     <div className="container">
@@ -31,13 +63,17 @@ export default function App() {
         <span className='nome-pokemon'>{nome}</span>
       </h1>
       
-      <form className='form'>
-        <input type="search" placeholder='nome ou número' className='input_pesquisa' id='input' required value={pesquisa}/>
+      <form className='form' onSubmit={(e) => {
+        e.preventDefault();
+        renderPokemon(pesquisa.toLowerCase());
+        setPesquisa('')
+      }}>
+        <input type="search" placeholder='nome ou número' className='input_pesquisa' id='pesquisa' value={pesquisa} onChange={handleChange}/>
       </form>
 
       <div className='buttons'>
-        <button className='btn'>Voltar &lt;</button>
-        <button className='btn'>Próximo &gt;</button>
+        <button className='btn' onClick={handleClickMenos}>Voltar &lt;</button>
+        <button className='btn'onClick={handleClickMais}>Próximo &gt;</button>
       </div>
 
       <img src={pokedex} alt="Pokedex" className='pokedex'/>
